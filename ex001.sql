89a02214a585d46934b67e2cd13618acc0570a40
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-03-01 16:09:00 +0100

    up

---
+++ ex001.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE cadastro;
+CREATE TABLE pessoas (
+nome varchar(30),
+idade tinyint(3),
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+SELECT * FROM pessoas;
