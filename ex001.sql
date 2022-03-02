4023678929527ad936d966be417debf7dbe62315
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-03-02 08:36:57 +0100

    up

--- ex001.sql
+++ ex001.sql
@@ -13,3 +13,9 @@ nacionalidade VARCHAR(20) DEFAULT 'Brasil',
 PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8;
 
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura)
+VALUE
+('Creusa', '1920-12-30', 'F', '50.2', '1.65');
+
+SELECT * FROM pessoas;
