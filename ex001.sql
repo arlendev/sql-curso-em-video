e74fa270802f3f4359b3777601da8bae236f4199
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-03-02 08:23:12 +0100

    up

--- ex001.sql
+++ ex001.sql
@@ -3,10 +3,13 @@ DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 
 CREATE TABLE pessoas (
-nome VARCHAR(30) not null,
+id INT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(30) NOT NULL,
 nascimento DATE,
 sexo enum('M', 'F'),
 peso DECIMAL(5,2),
 altura DECIMAL(3,2),
-nacionalidade VARCHAR(20) DEFAULT 'Brasil'
+nacionalidade VARCHAR(20) DEFAULT 'Brasil',
+PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8;
+
