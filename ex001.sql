5821662f25663f99e1949fb0ac3babcba449768b
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-03-08 14:29:59 +0100

    up

--- ex001.sql
+++ ex001.sql
@@ -159,26 +159,3 @@ SELECT carga, COUNT(*) FROM cursos
 WHERE ano > 2015
 GROUP BY carga
 HAVING carga > (SELECT AVG(carga) FROM cursos);
-
-
-SELECT * FROM gafanhotos;
-
-# 1) uma lista com as profissoes dos gafanhatos e seus respectivos quantitativos.
-SELECT profissao, COUNT(*) FROM gafanhotos
-GROUP BY profissao;
-
-#  2) Quantos gafanhotos homens e mulheres nasceram após 01/jan/2005 ?
-SELECT sexo, COUNT(*) FROM gafanhotos
-WHERE nascimento > '2005-01-01'
-GROUP BY sexo;
-
-/* 3) Lista com gafanhotos que nasceram fora do BRASIL, mostrando o país de origem
-e o total de pessoas nascidas lá. Só nos interessam os países que tiveram mais de 3
-gafanhotos com essa nacionalidade. */
-SELECT nacionalidade, COUNT(*) FROM gafanhotos
-WHERE nacionalidade != 'Brasil'
-GROUP BY nacionalidade
-HAVING COUNT(*) > '3';
-
-/* 4) Uma lista agrupada pela altura dos gafanhotos ,mostrando quantas pessoas 
-pesam mais de 100kg e que estao acima da media da altura de todos os gafanhotoso.*/
