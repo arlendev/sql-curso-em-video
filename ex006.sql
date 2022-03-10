5ab6941d156f94720b11334af3689376542a4fd3
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-03-10 13:42:37 +0100

    up

--- ex006.sql
+++ ex006.sql
@@ -14,3 +14,10 @@ SELECT * FROM gafanhoto_assiste_curso;
 
 INSERT INTO gafanhoto_assiste_curso VALUES
 (DEFAULT, '2014-03-01', '1', '2');
+
+SELECT g.nome, c.nome FROM gafanhotos AS g
+JOIN gafanhoto_assiste_curso AS a
+ON g.id = a.idgafanhoto
+JOIN cursos AS c
+ON c.idcurso = a.idcurso
+ORDER BY g.nome;
