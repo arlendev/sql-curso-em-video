81f58dbde89247c73f10136b1e6771b9abbbf9c5
Arlen POSSAMAI <77725374+arlendev@users.noreply.github.com>
2022-03-10 10:50:59 +0100

    up

--- ex006.sql
+++ ex006.sql
@@ -1,1 +1,16 @@
 SELECT * FROM gafanhotos;
+
+CREATE TABLE gafanhoto_assiste_curso (
+	id INT NOT NULL AUTO_INCREMENT,
+    DATA DATE,
+    idgafanhoto INT,
+    idcurso INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idgafanhoto) REFERENCES gafanhotos(id),
+    FOREIGN KEY (idcurso) REFERENCES cursos(idcurso)
+) DEFAULT CHARSET = utf8;
+
+SELECT * FROM gafanhoto_assiste_curso;
+
+INSERT INTO gafanhoto_assiste_curso VALUES
+(DEFAULT, '2014-03-01', '1', '2');
